96a837c274f6c9ec606c065efb08d3d9f6a65a60
James <ohbwgab18@gmail.com>
2025-08-23 00:22:37 +0800

    aos

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
 
-console.log('Hello world');
+console.log("Hello world");
+
+AOS.init({
+  offset: 120,
+  duration: 800,
+  once: false,
+  delay: 0,
+});
